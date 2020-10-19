e02841a4adb8838b5841fe61d1f339b97c5644d6
Fimblthulr <Freyja.walberg@t-online.de>
2020-10-19 22:05:46 +0200

    init

---
+++ inc/launch_game.h
@@ -0,0 +1,28 @@
+#ifndef LAUNCH_GAME_H
+#define LAUNCH_GAME_H
+
+/*
+ * =====================================================================================
+ *
+ *       Filename:  launch_game.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  19/10/20 20:33:51
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Freyja Walberg (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <unistd.h>
+
+pid_t
+launch_game
+	(char *argv);
+
+#endif /* LAUNCH_GAME_H */
